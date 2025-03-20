f2839fe4807bce7bfc79a638179bff74aaaf1d0b
aleorozcol <aorozcolopez@udesa.edu.ar>
2025-03-20 17:58:35 -0300

    ex3 readyy

--- Ejercicio3/ejercicio3.cpp
+++ Ejercicio3/ejercicio3.cpp
@@ -20,39 +20,45 @@ Presentar ejemplos que verifiquen el funcionamiento requerido en las funciones i
 muy importante para el ejercicio, sólo utilizar smart pointers. 
 */
 
+// struct del nodo de la lista enlazada
 struct Node {
     int value;
     shared_ptr<Node> next;
-    Node(int val): value(val), next(nullptr){}
+    Node(int val): value(val), next(nullptr){} // inicializa el nodo con un valor y un puntero nulo
 };
 
+// puntero global al inicio de la lista
 shared_ptr<Node> head = nullptr;
 
+// i. create_node(): devuelve un nodo.
 shared_ptr<Node> create_node(int value){
     return make_shared<Node>(value);
 }
 
+// ii. push_front(): inserta un nodo al frente de la lista.
 void push_front(int value) {
     auto new_node = create_node(value);
-    new_node->next = head;
-    head = new_node;
+    new_node->next = head; // el nuevo nodo apunta al anterior head
+    head = new_node; // actualizamos el head
 }
 
+// iii. push_back(): inserta un nodo al final de la lista.
 void push_back(int value){
     auto new_node =create_node(value);
-    if (!head) {
+    if (!head) { // lista vacía, entonces el nuevo nodo es head
         head = new_node;
         return;
     }
     auto current = head;
-    while (current->next){
+    while (current->next){ // recorremos hasta el ultimo nodo
         current = current->next;
     }
     current->next = new_node;
 }
 
+// iv. insert(): inserta un nodo en la posición que se le pase a la función.
 void insert(int value, int pos) {
-    if (pos == 0) {
+    if (pos == 0) { // primera posición
         push_front(value);
         return;
     }
@@ -61,13 +67,13 @@ void insert(int value, int pos) {
     auto current = head;
     int index = 0;
 
-    while (current && index < pos-1){
+    while (current && index < pos-1){ // avanzamos hasta la pos que queremos o hasta el final
         current = current->next;
         index++;
     }
 
-    if (!current) {
-        cout << "Lo inserté al final porque se pasó del largo de la lista.\n";
+    if (!current) { // la pos es mayor al tamaño de la lista entonces lo insertamos al final
+        cout << "El nodo fue insertado al final de la lista porque la posición elegida es mayor al tamaño de la lista.\n";
         push_back(value);
     } else {
         new_node-> next = current->next;
@@ -75,26 +81,32 @@ void insert(int value, int pos) {
     }
 }
 
+// v. erase(): borra un nodo en la posición que se le pase a la función.
 void erase(int pos){
+
     if (!head){
         cout << "La lista está vacía.\n";
         return;
     }
-    if (pos==0){
+
+    if (pos==0){ // actualizamos el head
         head = head->next;
         return;
     }
+
     auto current = head;
     int index = 0;
-    while (current->next && index < pos-1){
+    
+    while (current->next && index < pos-1){ // avanzamos hasta el nodo anterior
         current = current->next;
         index++;
     }
+
     if (!current->next){
-        cout << "Pos mayor, se borró el ult nodo.\n";
         if (head->next == nullptr){
             head = nullptr;
         } else {
+            cout << "Se borró el último nodo porque la posición se excede del tamaño de la lista.\n";
             auto temp = head;
             while (temp->next && temp->next->next){
                 temp = temp->next;
@@ -106,7 +118,11 @@ void erase(int pos){
     }
 }
 
+//vi. print_list(): imprime la lista completa.
 void print_list(){
+    if (!head){
+        cout << "La lista está vacía.\n";
+    }
     auto current = head;
     while (current){
         cout << current->value;
@@ -119,8 +135,42 @@ void print_list(){
 }
 
 int main(){
-    push_front(10);
-    push_front(20);
+    // Test de push_front
+    cout << "Insert de 3 veces un head (push_front):\n";
     push_front(30);
+    push_front(20);
+    push_front(10);
+    print_list();
+    // Test de push_back
+    cout << "Insert de 3 veces un tail (push_back):\n";
+    push_back(40);
+    push_back(50);
+    push_back(60);
+    print_list();
+    // Test de insert
+    cout << "Insert 2 veces en posiciones dentro de la lista y 1 fuera (insert):\n";
+    insert(15,1);
+    insert(25,3);
+    insert(70,15);
+    print_list();    
+    //Test de erase
+    cout << "Erase de toda la lista:\n";
+    erase(0);
+    print_list();
+    erase(0);
+    print_list();
+    erase(0);
+    print_list();
+    erase(0);
+    print_list();
+    erase(0);
+    print_list();
+    erase(0);
+    print_list();
+    erase(0);
+    print_list();
+    erase(0);
+    print_list();
+    erase(0);
     print_list();
 }
